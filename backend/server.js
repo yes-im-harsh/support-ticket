b649be8e6e5583f2fa46dc6b092921a61bc7fe81
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-07-27 21:36:49 +0530

    Basic Express Server Setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log("Server ...");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.get("/api/users", (req, res) => {
+  res.status(200).json({ message: "Welcome to the support desk api" });
+});
+app.listen(PORT, () => console.log(`Server Started on ${PORT}`));
