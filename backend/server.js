f2fd1dd4cf6cb0b8fbb8202b2ecd02288089aed7
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-07-28 15:38:35 +0530

    Add Routes and Controller

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+
 const PORT = process.env.PORT || 8000;
 
 const app = express();
@@ -7,4 +8,8 @@ const app = express();
 app.get("/api/users", (req, res) => {
   res.status(200).json({ message: "Welcome to the support desk api" });
 });
+
+//Routes
+app.use("/api/users", require("./routes/userRoutes"));
+
 app.listen(PORT, () => console.log(`Server Started on ${PORT}`));
