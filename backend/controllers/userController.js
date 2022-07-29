5b7c8360d1cedff6d2368e2930e182113ac8a49a
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-07-29 20:26:18 +0530

    Register User

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,7 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+
+const User = require("../models/userModel");
 
 //@desc Register a new user
 //@route /api/users
@@ -11,7 +14,36 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Must Include all fields");
   }
-  res.send("Register User");
+
+  //find if user exists
+  const userExists = await User.findOne({ email });
+
+  //Hash Password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  //Create User
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid User Data");
+  }
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
 });
 
 //@desc Register a new user
