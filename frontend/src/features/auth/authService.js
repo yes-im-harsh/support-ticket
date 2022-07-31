6e04a6512f2f0a268d604e514f08056659cccec4
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-07-31 23:46:50 +0530

    Logout User

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -14,6 +14,9 @@ const register = async (userData) => {
   return response.data;
 };
 
-const authService = { register };
+//Logout user
+const logout = () => localStorage.removeItem("user");
+
+const authService = { register, logout };
 
 export default authService;
--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -35,6 +35,11 @@ export const login = createAsyncThunk("/auth/login", async (user, thunkAPI) => {
   console.log(user);
 });
 
+//Logout user
+export const logout = createAsyncThunk("/auth/logout", async () => {
+  authService.logout();
+});
+
 export const authSlice = createSlice({
   name: "auth",
   initialState,
@@ -60,6 +65,9 @@ export const authSlice = createSlice({
         state.isLoading = false;
         state.isError = true;
         state.message = action.payload;
+        state.user = null;
+      })
+      .addCase(logout.fulfilled, (state) => {
         state.user = null;
       });
   },
