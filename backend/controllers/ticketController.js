95287c346ed4ebdf3e10661de742df1b1ad7f7f0
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-08-01 21:03:15 +0530

    Single Ticket, Update, & Delete (Backend). Full CRED Functionality Implemented!

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -19,6 +19,33 @@ const getTickets = asyncHandler(async (req, res) => {
   res.status(200).json(tickets);
 });
 
+//@desc Get user Ticket
+//@route GET /api/tickets/:id
+//@access Private
+const getTicket = asyncHandler(async (req, res) => {
+  //Get user using the id in JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User Not found");
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not found");
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not authorized");
+  }
+
+  res.status(200).json(ticket);
+});
+
 //@desc Create new tickets
 //@route POST /api/tickets
 //@access Private
@@ -48,7 +75,71 @@ const createTicket = asyncHandler(async (req, res) => {
   res.status(201).json(ticket);
 });
 
+//@desc   Delete Ticket
+//@route  DELETE /api/tickets/:id
+//@access Private
+const deleteTicket = asyncHandler(async (req, res) => {
+  //Get user using the id in JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User Not found");
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not found");
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not authorized");
+  }
+
+  await ticket.remove();
+  res.status(200).json({ success: true });
+});
+
+//@desc  Update Ticket
+//@route PUT /api/tickets/:id
+//@access Private
+const updateTicket = asyncHandler(async (req, res) => {
+  //Get user using the id in JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User Not found");
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not found");
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not authorized");
+  }
+
+  const updatedTicket = await Ticket.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+
+  res.status(200).json(updatedTicket);
+});
+
 module.exports = {
   getTickets,
+  getTicket,
   createTicket,
+  deleteTicket,
+  updateTicket,
 };
