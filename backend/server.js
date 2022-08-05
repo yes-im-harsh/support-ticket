7a8559dea463c7e327574f3a7a88c33ec208fbd0
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-08-05 23:28:29 +0530

    Prepare To Deploy

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,12 @@ app.use("/api/tickets", require("./routes/ticketRoutes"));
 
 //Serve Frontend
 if (process.env.NODE_ENV === "production") {
+  //Set build folder as static
   app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(__dirname, "../", "frontend", "build", "index.html");
+  });
 }
 
 app.use(errorHandler);
